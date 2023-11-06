a5654121ba7a75bc8c0af87cd0de1c52a5f8b1d2
Dmitry Akatov <dmitry.akatov@protonmail.com>
2023-11-06 23:04:08 +0200

    pretty print and compaction

--- src/automaton.py
+++ src/automaton.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+import functools as ft
+
+import math
 from typing import *
 from dataclasses import dataclass
 
@@ -9,20 +12,40 @@ class Char:
 
 
 class Node:
-    children: dict[str, Node]
+    children: dict[str, IntermediateNode]
 
     def __init__(self) -> None:
         self.children = dict()
 
-    def __repr__(self) -> str:
-        result = []
-        for child in self.children:
-            result.append(f' - {child}')
-        return '*' + '\n '.join(result)
-
 
 class RootNode(Node):
-    ...
+    def pretty(self) -> list[str]:
+        ROOT_SYMBOL = '*'
+        TAB_SYMBOL = ' '
+        TOKEN_LEN = 6
+
+        result = [ROOT_SYMBOL]
+        prev_depth = math.inf
+
+        stack = [(1, child) for child in self.children.values()]
+
+        while stack:
+            depth, node = stack.pop()
+
+            if prev_depth >= depth:
+                result.append('\n' + TAB_SYMBOL * depth)
+
+            prev_depth = depth
+            match node:
+                case EndNode():
+                    result.append(node.value + '!' + f'({node.depth})')
+                case _:
+                    result.append(node.value + ' ' + f'({node.depth})')
+
+            for child in node.children.values():
+                stack.append((depth + TOKEN_LEN, child))
+
+        return result
 
 
 class IntermediateNode(Node):
@@ -45,15 +68,33 @@ class Automaton:
     def __init__(self) -> None:
         self.root = RootNode()
 
-    def add_word(self, word: str) -> None:
-        current_node: Node = self.root
-        depth = len(word)
+    @ft.cache
+    def normalize_word(self, word: str) -> str:
+        if len(word) <= 3:
+            return word
 
-        for index, char in enumerate(word):
+        return word[0] + ''.join(sorted(word[1:len(word) - 1])) + word[-1]
+
+    def add_word(self, word: str) -> None:
+        normalized_word = self.normalize_word(word)  # compaction
+
+        current_node: Node = self.root
+        depth = len(normalized_word)
+
+        for index, char in enumerate(normalized_word):
+            is_last_node = index == depth - 1
+
             if char in current_node.children:
+
                 match current_node:
                     case IntermediateNode():
                         current_node.depth = max(current_node.depth, depth)
+
+                if is_last_node:
+                    match current_node.children[char]:
+                        case IntermediateNode() as im:
+                            current_node.children[char] = EndNode(char, max(im.depth, depth))
+                            current_node.children[char].children = im.children
 
                 current_node = current_node.children[char]
             else:
@@ -67,11 +108,18 @@ class Automaton:
                 current_node.children[char] = new_node
                 current_node = new_node
 
-
 automaton = Automaton()
 automaton.add_word('aapxj')
 automaton.add_word('apaxj')
+automaton.add_word('apaxd')
+automaton.add_word('apal')
+automaton.add_word('pda')
 automaton.add_word('bpd')
+automaton.add_word('bp')
+automaton.add_word('bzd')
+automaton.add_word('b')
+
+print(' '.join(automaton.root.pretty()))
 
 # class MTACAutomaton:
 #     alphabet: int
